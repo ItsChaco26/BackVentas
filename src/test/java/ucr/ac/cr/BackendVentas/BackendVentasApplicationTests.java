012ebc3e4efa6a1651b0cf9b7d3203a8b172c682
ItsChaco26 <aaronchacon2601@gmail.com>
2025-03-26 14:44:16 -0600

    Initial commit

---
+++ src/test/java/ucr/ac/cr/BackendVentas/BackendVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.BackendVentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
